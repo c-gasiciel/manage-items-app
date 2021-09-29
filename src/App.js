735fb0b0ba11154a5924f725eb5ea1147bd2f457
c-gasiciel <clarissa.gasiciel@gmail.com>
2021-09-28 21:47:07 -0500

    Style list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import Header from './components/Header';
 import List from './components/List';
 import './App.css';
 
 const App = (props) => {
+  /* Set up array for list inputs */
+  const [ myList, setMyList ] = useState([
+    "Clean", 
+    "Read", 
+    "Finish this project"
+  ]);
+
   return (
     <div className="App">
       <Header pageTitle="Manage My Items" />
       <main>
         <Form />
-        <List />
+        <List display={ myList } />
       </main>
     </div>
   );
 }
 
 export default App;
+ 
--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const List = (props) =>{
     return(
-        <section>
-            <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            </ul>
-        </section>
+        <ListContainer>
+            <UnorderedList>
+                <ListItemCard>Item 1</ListItemCard>
+                <ListItemCard>Item 2</ListItemCard>
+                <ListItemCard>Item 3</ListItemCard>
+            </UnorderedList>
+        </ListContainer>
     );
 }
 
 export default List;
+
+
+const ListContainer = styled.section` 
+    background: #DDC9A2;
+    margin: .5em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const UnorderedList = styled.ul` 
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    list-style: none;
+    width: 60%;
+`;
+
+
+const ListItemCard = styled.li`
+    background: #C28EA0;
+    box-sizing: border-box;
+    margin: 1em 0em;
+    padding: .5em 1em;
+    width: 60%;
+`;
+
